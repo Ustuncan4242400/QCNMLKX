493ac6f9e451c1f5672eaa745a24776551ffd27f
Ustuncan4242400 <59260929+Ustuncan4242400@users.noreply.github.com>
2020-01-15 20:06:48 +0300

    Add files via upload

---
+++ BEHT.sh
@@ -0,0 +1,412 @@
+#! bin/bash
+#CODED BY STAR_SKY
+echo -e '
+________________$$$$$$$$$$$$$$__________________________________ 
+'
+sleep 0.1
+echo -e '
+_____________$$$$$$$$$$$$$$$$$_________________________________ 
+'
+sleep 0.1
+echo -e '
+____________$$$$$$$$$$$$$$$$$$$________________________________ 
+'
+sleep 0.1
+echo -e '
+__________$$$$$$$$$$$$$$$$$$$$$$$______________________________ 
+'
+sleep 0.1
+echo -e '
+__________$$$$$$$$$$$$$$$$$$$$$$$$_____________________________ 
+'
+sleep 0.1
+echo -e '
+________$$$$$$$$$$$$$$$_____$$$$$$$____________________________ 
+'
+sleep 0.1
+echo -e '
+________$$$$$$$$$$$$___________$$$$____________________________ 
+'
+sleep 0.1
+echo -e '
+_______$$$$$$$$$$$$_____________$$$$___________________________ 
+'
+sleep 0.1
+echo -e '
+_______$$$$$$$$$$$_______________$$$___________________________ 
+'
+sleep 0.1
+echo -e '
+______$$$$$$$$$$$_________________$$$_____$$___________________ 
+'
+sleep 0.1
+echo -e '
+______$$$$$$$$$$__________________________$$$__________________ 
+'
+sleep 0.1
+echo -e '
+______$$$$$$$$$___________________________$$$__________________ 
+'
+sleep 0.1
+echo -e '
+_____$$$$$$$$$$___________________________$$$$_________________ 
+'
+sleep 0.1
+echo -e '
+_____$$$$$$$$$____________________________$$$$$$$$$$$$_________ 
+'
+sleep 0.1
+echo -e '
+_____$$$$$$$$$___________________________$$$$$$$$$$$___________ 
+'
+sleep 0.1
+echo -e '
+_____$$$$$$$$$_________________________$$$$$$$$$$$$____________ 
+'
+sleep 0.1
+echo -e '
+_____$$$$$$$$$_______________________$$$$$$$$$$$$$_____________ 
+'
+sleep 0.1
+echo -e '
+_____+$$$$$$$$_____________________$$$$$$$$$$$$$$$_____________ 
+'
+sleep 0.1
+echo -e '
+_____$$$$$$$$$_________________________$$$$$$$$$$$_____________
+'
+sleep 0.1
+echo -e '
+_____$$$$$$$$$___________________________$$$$$$$$$$____________ 
+'
+sleep 0.1
+echo -e '
+_____$$$$$$$$$$___________________________$$$$$$$$$$___________ 
+'
+sleep 0.1
+echo -e '
+______$$$$$$$$$___________________________$$$$$$$$$$___________ 
+'
+sleep 0.1
+echo -e '
+______$$$$$$$$$___________________________$$$$____$$$__________ 
+'
+sleep 0.1
+echo -e '
+_______$$$$$$$$$__________________________$$$_______$$_________ 
+'
+sleep 0.1
+echo -e '
+_______$$$$$$$$$$_________________$_______$$__________$________ 
+'
+sleep 0.1
+echo -e '
+________$$$$$$$$$$_______________$$_______$____________________ 
+'
+sleep 0.1
+echo -e '
+________$$$$$$$$$$$_____________$$$____________________________ 
+'
+sleep 0.1
+echo -e '
+_________$$$$$$$$$$$$$________$$$$_____________________________ 
+'
+sleep 0.1
+echo -e '
+__________$$$$$$$$$$$$$$$$$$$$$$$______________________________ 
+'
+sleep 0.1
+echo -e '
+___________$$$$$$$$$$$$$$$$$$$$$_______________________________ 
+'
+sleep 0.1
+echo -e '
+____________$$$$$$$$$$$$$$$$$$$________________________________ 
+'
+sleep 0.1
+echo -e '
+_____________$$$$$$$$$$$$$$$$$_________________________________ 
+'
+sleep 0.1
+echo -e '
+______________$$$$$$$$$$$$$$$__________________________________ 
+'
+sleep 0.1
+echo -e '
+_______________$$$$$$$$$$$$$___________________________________
+'
+sleep 0.1
+echo -e '
+  (+ (+ (+ (+ (+ BROKEN  EAGLE HACK TEAM +) +) +) +) +)
+    			'
+echo -e '\033[31;40;1m
+0=GEREKLİ KURULUMLAR
+'
+echo -e '\e[32m
+------------------------------------------------------------------¦
+ ____   ___  ____     ___     ____   ___ _____ _   _ _____ _____  ¦
+|  _ \ / _ \/ ___|   ( _ )   | __ ) / _ \_   _| \ | | ____|_   _| ¦
+| | | | | | \___ \   / _ \/\ |  _ \| | | || | |  \| |  _|   | |   ¦
+| |_| | |_| |___) | | (_>  < | |_) | |_| || | | |\  | |___  | |   ¦
+|____/ \___/|____/   \___/\/ |____/ \___/ |_| |_| \_|_____| |_|   ¦
+
+------------------------------------------------------------------¦
+1=ufonet
+2=TORSHAMMER
+3=HAMMER
+4=T-DDos
+5=DDOS-BOTNET
+'     
+echo -e '\e[35m
+--------------------------------------¦
+ ____  _   _ ___ ____ ___ _   _  ____ ¦
+|  _ \| | | |_ _/ ___|_ _| \ | |/ ___|¦
+| |_) | |_| || |\___ \| ||  \| | |  _ ¦
+|  __/|  _  || | ___) | || |\  | |_| |¦
+|_|   |_| |_|___|____/___|_| \_|\____|¦
+--------------------------------------¦
+6=TURK-SPLOİT DEMO
+7=TURK-SPLOİT
+8=PHİSX
+9=SHELLPHİSH
+10=PHİSİNG SOCİAL
+11=HİDDENEYE
+12=GHOST-phisher
+'    
+echo -e '\e[36m
+--------------------------------¦
+ ____  ____  _   _ _____ _____  ¦
+| __ )||  _ \| | | |_   _| ____|¦
+|  _ \| |_) | | | | | | |  _|   ¦ 
+| |_) |  _ <| |_| | | | | |___  ¦
+|____/|_| \_\\___/  |_| |_____| ¦
+--------------------------------¦
+
+13=İNSTASHELL
+14=İNSTAİNSANE
+15=INSTABRUTE
+16=FACEBASH
+17=TWEETSHELL
+18=GMAİL HACK
+19=WORDPRESS BRUTE
+'
+echo -e '\033[31;40;1m
+---------------------------------------------------
+__        _______ ____    _   _    _    ____ _  __¦
+\ \      / / ____| __ )  | | | |  / \  / ___| |/ /¦
+ \ \ /\ / /|  _| |  _ \  | |_| | / _ \| |   |   / ¦
+  \ V  V / | |___| |_) | |  _  |/ ___ \ |___|   \ ¦
+   \_/\_/  |_____|____/  |_| |_/_/   \_\____|_|\_\¦
+--------------------------------------------------¦
+
+20=SQLMAP
+21=KO-DORK
+22=RED-HAWK
+23=LITESCRİPT
+'
+echo -e '\e[34m
+---------------------------------------------------¦
+ ____  ____   _    __  __   _____ ___  _     ____  ¦
+/ ___||  _ \ / \  |  \/  | |_   _/ _ \| |   / ___| ¦
+\___ \| |_) / _ \ | |\/| |   | || | | | |   \___ \ ¦
+ ___) |  __/ ___ \| |  | |   | || |_| | |___ ___) |¦
+|____/|_| /_/   \_\_|  |_|   |_| \___/|_____|____/ ¦
+---------------------------------------------------¦
+
+24=T-BOMB(SMS SPAM=)
+25=İNSTASPAM
+26=İNSTASPAM V.2
+27=İNSTASPAM (CHOİCE BOT)
+28=TELEGRAM BOT
+
+
+99=EXİT
+'
+read -p 'İŞLEM SEÇİNİZ=>' secim
+
+if [[ $secim == 1 || $secim == 01 ]]; then 			
+  	cd $HOME
+	git clone https://github.com/cyweb/hammer
+	cd $HOME/QCNMLKX
+	bash BEHT.sh
+			
+elif [[ $secim == 2 || $secim == 02 ]]; then 
+	cd $HOME
+	git clone https://github.com/dotfighter/torshammer
+	cd $HOME/QCNMLKX
+	bash BEHT.sh
+               
+elif [[ $secim == 3 || $secim == 03 ]]; then
+	cd $HOME
+	git clone https://github.com/COLAK-C/DDoS-Tool
+	cd QCNMLKX
+	bash BEHT.sh
+
+elif [[ $secim == 4 || $secim == 04 ]]; then
+	cd $HOME  
+	git clone https://github.com/yamanefkar/T-DDos
+	cd QCNMLKX
+	bash BEHT.sh
+		   		   
+elif [[ $secim == 5 || $secim == 05 ]]; then
+	cd $HOME
+	git clone https://github.com/LyashenkoGS/DDOS-botnet
+	cd QCNMLKX
+	bash BEHT.sh
+		   
+elif [[ $secim == 6 || $secim == 06 ]]; then
+	cd $HOME
+	git clone https://github.com/yamanefkar/Turk-Sploit-Demo
+	cd QCNMLKX
+	bash BEHT.sh
+		   
+elif [[ $secim == 7 || $secim == 07 ]]; then
+	cd $HOME
+	git clone https://github.com/yamanefkar/Turk-Sploit
+	cd QCNMLKX
+	bash BEHT.sh
+				   
+elif [[ $secim == 8 || $secim == 08 ]]; then
+	cd $HOME
+	git clone https://github.com/muhammedxyilmaz/PhishX
+	cd QCNMLKX
+	bash BEHT.sh
+		    
+elif [[ $secim == 9 ]]; then
+	cd $HOME
+	git clone https://github.com/thelinuxchoice/shellphish
+	cd QCNMLKX
+	bash BEHT.sh
+		    
+elif [[ $secim == 10 ]]; then
+	cd $HOME
+	git clone https://github.com/xHak9x/SocialPhish
+	cd QCNMLKX
+	bash BEHT.sh
+
+elif [[ $secim == 11 ]]; then
+	cd $HOME
+	git clone https://github.com/DarkSecDevelopers/HiddenEye
+	cd QCNMLKX
+	bash BEHT.sh
+
+elif [[ $secim == 12 ]]; then
+	cd $HOME
+	git clone https://github.com/savio-code/ghost-phisher
+	cd QCNMLKX
+	bash BEHT.sh
+
+elif [[ $secim == 13 ]]; then
+	cd $HOME
+	git clone https://github.com/thelinuxchoice/instashell
+	cd QCNMLKX
+	bash BEHT.sh
+
+elif [[ $secim == 14 ]]; then
+	cd $HOME
+	git clone https://github.com/thelinuxchoice/instainsane
+	cd QCNMLKX
+	bash BEHT.sh
+
+elif [[ $secim == 15 ]]; then 
+	cd $HOME
+	git clone https://github.com/Ha3MrX/InstaBrute
+	cd QCNMLKX
+	bash BEHT.sh
+		    
+elif [[ $secim == 16 ]]; then
+	cd $HOME
+	git clone https://github.com/github.com/thelinuxchoice/facebash
+	cd QCNMLKX
+	bash BEHT.sh
+
+elif [[ $secim == 17 ]]; then
+	cd $HOME
+	git clone https://github.com/thelinuxchoice/tweetshell
+	cd QCNMLKX
+	bash BEHT.sh
+
+elif [[ $secim == 18 ]]; then
+	cd $HOME
+	git clone https://github.com/xHak9x/gmailhack
+	cd QCNMLKX
+	bash BEHT.sh
+
+elif [[ $secim == 19 ]]; then
+	cd $HOME
+	git clone https://github.com/RecepGunes01/WordPress-Brute-Force
+	cd QCNMLKX
+	bash BEHT.sh
+
+elif [[ $secim == 20 ]]; then
+	cd $HOME
+	git clone https://github.com/sqlmapproject/sqlmap
+	cd $HOME/QCNMLKX
+	bash BEHT.sh
+
+elif [[ $secim == 21 ]]; then
+	cd $HOME	
+	git clone https://github.com/CiKu370/ko-dork
+	cd $HOME/QCNMLKX
+	bash BEHT.sh
+
+elif [[ $sscim == 22 ]]; then
+	cd $HOME
+	git clone https://github.com/Tuhinshubhra/RED_HAWK
+	cd $HOME/QCNMLKX
+	bash BEHT.sh
+
+elif [[ $secim == 23 ]]; then
+	cd $HOME
+	git clone https://github.com/4L13199/LITESCRIPT
+	cd $HOME/QCNMLKX
+	bash BEHT.sh
+
+elif [[ $secim == 24 ]]; then
+	cd $HOME
+	git clone https://github.com/TheSpeedX/TBomb
+	cd $HOME/QCNMLKX
+	bash BEHT.sh
+			
+elif [[ $secim == 25 ]]; then
+	cd $HOME
+	git clone https://github.com/tarik0/instaspam
+	cd QCNMLKX
+	 bash BEHT.sh
+
+elif [[ $secim == 26 ]]; then
+	cd $HOME
+	git clone https://github.com/tarik0/instaspamv2.1
+	cd QCNMLKX
+	bash BEHT.sh
+
+elif [[ $secim == 27 ]]; then
+	cd $HOME
+	git clone https://github.com/thelinuxchoice/choicebot
+	cd QCNMLKX
+	bash BEHT.sh
+
+elif [[ $secim == 28 ]]; then
+	cd $HOME
+	git clone https://github.com/TelegramBots/Telegram.Bot
+	cd QCNMLKX
+	bash BEHT.sh
+
+elif [[ $secim == 99 ]]; then
+	clear
+	echo -e 'BEHT HACK TEAM İYİ GÜNLER DİLER^_^'
+	exit
+
+else
+	clear
+        echo -e '
+ _   _    _  _____  _
+| | | |  / \|_   _|/ \
+| |_| | / _ \ | | / _ \
+|  _  |/ ___ \| |/ ___ \
+|_| |_/_/   \_\_/_/   \_\
+'
+	sleep 3.5
+	bash BEHT.sh
+fi
+						
